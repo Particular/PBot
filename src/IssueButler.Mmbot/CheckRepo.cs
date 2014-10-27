eaa90f6b57f4cbf4596068658d53b82374688c9f
Andreas Öhlund <andreas.ohlund@particular.net>
2014-10-27 14:02:27 +0100

    Added a first mmbot hello world

---
+++ src/IssueButler.Mmbot/CheckRepo.cs
@@ -0,0 +1,19 @@
+﻿namespace IssueButler.Mmbot
+{
+    using System.Collections.Generic;
+    using MMBot;
+    using MMBot.Scripts;
+
+    public class CheckRepo:IMMBotScript
+    {
+        public void Register(Robot robot)
+        {
+            robot.Respond(@"check$", msg => msg.Send("Hello from IssueButler"));
+        }
+
+        public IEnumerable<string> GetHelp()
+        {
+            yield return "Checks a repo for issue in needs of some TLC";
+        }
+    }
+}
