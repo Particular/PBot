22b32c141d248ec58f348d629a84e282df35335d
Andreas Öhlund <andreas.ohlund@particular.net>
2014-11-14 22:20:40 +0100

    Updates color for existing labels

--- src/IssueButler.Tests/UpdateRepoLabelsTests.cs
+++ src/IssueButler.Tests/UpdateRepoLabelsTests.cs
@@ -25,13 +25,26 @@
             var existingLabels = client.Issue.Labels.GetForRepository(Organization, repoToUpdate).Result;
 
             
-            foreach (var label in labelsToSync)
+            foreach (var templateLabel in labelsToSync)
             {
-                if (!existingLabels.Any(existing => existing.Name == label.Name))
+                var existingLabel = existingLabels.SingleOrDefault(l => l.Name == templateLabel.Name);
+
+                if (existingLabel == null)
                 {
-                    Console.Out.WriteLine("{0} doesn't exist and will be created",label.Name);
-                    client.Issue.Labels.Create(Organization, repoToUpdate, new NewLabel(label.Name, label.Color)).Wait();
+                    Console.Out.WriteLine("{0} doesn't exist and will be created",templateLabel.Name);
+                    client.Issue.Labels.Create(Organization, repoToUpdate, new NewLabel(templateLabel.Name, templateLabel.Color)).Wait();
                 }
+                else
+                {
+                    if (existingLabel.Color != templateLabel.Color)
+                    {
+                        Console.Out.WriteLine("{0} has non matching color, will be updated",existingLabel.Name);
+                        client.Issue.Labels.Update(Organization, repoToUpdate,existingLabel.Name, new LabelUpdate(templateLabel.Name, templateLabel.Color))
+                            .Wait();
+                    }
+                }
+
+                //
             }
      
         }
