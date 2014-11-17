52ac7578ddbb3ffd77dc22621d3689b2e211ba8e
Andreas Öhlund <andreas.ohlund@particular.net>
2014-11-17 21:36:17 +0100

    Remove old butler code

--- src/IssueButler/ResultDisplayer.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace IssueButler
-{
-    using System.Collections.Generic;
-
-    public abstract class ResultDisplayer
-    {
-        public abstract void Display(IEnumerable<ValidationError> result);
-    }
-}
