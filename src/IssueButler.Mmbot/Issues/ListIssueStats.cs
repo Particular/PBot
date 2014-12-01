8a85701d11c6406cbe7db443c1bf33d2446e82bb
Andreas Öhlund <andreas.ohlund@particular.net>
2014-12-01 16:13:09 +0100

    Adding the current 7 day trend to the issue stats

--- src/IssueButler.Mmbot/Issues/ListIssueStats.cs
+++ src/IssueButler.Mmbot/Issues/ListIssueStats.cs
@@ -1,5 +1,6 @@
 namespace IssueButler.Mmbot.Issues
 {
+    using System;
     using System.Linq;
     using System.Threading.Tasks;
     using IssueButler.Mmbot.Repositories;
@@ -18,13 +19,13 @@ namespace IssueButler.Mmbot.Issues
                 .Select(r => GetRepoStats(r.Name))
                 .ToList();
 
-            await response.Send("This is the current issue stats: total(bugs)").IgnoreWaitContext();
-            await response.Send(string.Format("*All repos* - {0}({1})", stats.Sum(s => s.NumIssues), stats.Sum(s => s.NumBugs))).IgnoreWaitContext();
+            await response.Send("This is the current issue stats: total(trend) bugs").IgnoreWaitContext();
+            await response.Send(string.Format("*All repos* - {0}({2}) B:{1}", stats.Sum(s => s.NumIssues), stats.Sum(s => s.NumBugs), stats.Sum(s => s.Trend))).IgnoreWaitContext();
 
             foreach (var stat in stats.Where(s => s.NumIssues > 0)
                 .OrderByDescending(s => s.NumIssues))
             {
-                await response.Send(string.Format("*{2}* - {0}({1})", stat.NumIssues, stat.NumBugs, stat.RepoName)).IgnoreWaitContext();
+                await response.Send(string.Format("*{2}* - T:{0}({3}), Bugs:{1}", stat.NumIssues, stat.NumBugs, stat.RepoName,stat.Trend)).IgnoreWaitContext();
             }
 
             await response.Send("Hey, why don't you go ahead and fix some! Use `pbot check repo {name of repo above}` to find a few").IgnoreWaitContext();
@@ -32,16 +33,23 @@ namespace IssueButler.Mmbot.Issues
 
         RepoStats GetRepoStats(string name)
         {
+            var cutOffDate = DateTimeOffset.UtcNow.AddDays(-7);
             var client = GitHubClientBuilder.Build();
 
             var issues = client.Issue.GetForRepository("Particular", name, new RepositoryIssueRequest { State = ItemState.Open })
                 .Result;
 
+            var trend = client.Issue.GetForRepository("Particular", name, new RepositoryIssueRequest { State = ItemState.All, Since = cutOffDate })
+              .Result
+              .Where(i => i.CreatedAt >= cutOffDate || i.ClosedAt >= cutOffDate);
+
             return new RepoStats
             {
                 RepoName = name,
                 NumBugs = issues.Count(i => i.Labels.Any(l => l.Name == "Bug")),
-                NumIssues = issues.Count
+                NumIssues = issues.Count,
+                Trend = trend.Count(i => i.State == ItemState.Open) - trend.Count(i => i.State == ItemState.Closed)
+
             };
         }
     }
@@ -51,5 +59,6 @@ namespace IssueButler.Mmbot.Issues
         public string RepoName;
         public int NumIssues;
         public int NumBugs;
+        public int Trend;
     }
 }
