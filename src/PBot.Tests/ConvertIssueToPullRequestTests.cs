1e1725a22dfceba3477d67a2b93980dac87d0adc
SzymonPobiega <szymon.pobiega@gmail.com>
2014-12-04 05:59:29 +0100

    Added 'convert issue to pull request' command

---
+++ src/PBot.Tests/ConvertIssueToPullRequestTests.cs
@@ -0,0 +1,16 @@
+﻿namespace PBot.Tests
+{
+    using NUnit.Framework;
+    using PBot.Issues;
+
+    [TestFixture]
+    public class ConvertIssueToPullRequestTests : BotCommandFixture<ConvertIssueToPullRequest>
+    {
+        [Test]
+        [Explicit]
+        public void CanConvertAnExistingIssue()
+        {
+            Execute("convert issue", "64", "from repository", "NServiceBus.SqlServer", "into pull request from", "semaphore-based-throttling", "to", "develop");
+        }
+    }
+}
---
+++ src/PBot/Issues/ConvertIssueToPullRequest.cs
@@ -0,0 +1,53 @@
+﻿namespace PBot.Issues
+{
+    using System.Threading.Tasks;
+    using Octokit;
+
+    public class ConvertIssueToPullRequest : BotCommand
+    {
+        public ConvertIssueToPullRequest()
+            : base("(convert issue) (.*) (from repository) (.*) (into pull request from) (.*) to (.*)$",
+            "pbot convert issue <issue number> from repository <name of repo> into pull request from <PR branch> to <target branch> - Converts an issue into a pull request")
+        {
+        }
+
+        public override async Task Execute(string[] parameters, IResponse response)
+        {
+            var client = GitHubClientBuilder.Build();
+            client.Credentials = GitHubHelper.Credentials;
+            var apiConnection = new ApiConnection(client.Connection);
+
+            int issueNumber;
+            if (!int.TryParse(parameters[1], out issueNumber))
+            {
+                await response.Send("Issue number should be a valid number dude!");
+                return;
+            }
+            try
+            {
+                var result = await apiConnection.Post<PullRequest>(ApiUrls.PullRequests("Particular", parameters[3]), new ConvertedPullRequest
+                {
+                    Issue = issueNumber.ToString(),
+                    Head = parameters[5],
+                    Base = parameters[7]
+                });
+                await response.Send(string.Format("Issue {0} has been converted into a pull request {1}.", issueNumber, result.HtmlUrl));
+            }
+            catch (NotFoundException)
+            {
+                response.Send("Sorry, GitHub could not find the issue or repository you are talking about.").ConfigureAwait(false).GetAwaiter().GetResult();
+            }
+            catch (ApiValidationException)
+            {
+                response.Send("Sorry, your request was rejected by GitHub as invalid.").ConfigureAwait(false).GetAwaiter().GetResult();
+            }
+        }
+
+        public class ConvertedPullRequest
+        {
+            public string Issue { get; set; }
+            public string Head { get; set; }
+            public string Base { get; set; }
+        }
+    }
+}
