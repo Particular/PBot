ba2fd1dba81b720c313da119acbcd6bef03c6c42
Andreas Öhlund <andreas.ohlund@particular.net>
2014-12-09 21:35:22 +0100

    Adding support for requirements reminders

---
+++ src/PBot.Tests/RemindProductRoadmapTeamTests.cs
@@ -0,0 +1,16 @@
+﻿namespace PBot.Tests
+{
+    using NUnit.Framework;
+    using PBot.Reminders;
+
+    [TestFixture]
+    public class RemindProductRoadmapTeamTests : BotCommandFixture<RemindProductRoadmapTeam>
+    {
+        [Test]
+        public void IntegrationTest()
+        {
+            Execute("remind product roadmap team");
+        }
+
+    }
+}
---
+++ src/PBot/Reminders/RemindProductRoadmapTeam.cs
@@ -0,0 +1,43 @@
+﻿namespace PBot.Reminders
+{
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Octokit;
+
+    public class RemindProductRoadmapTeam : BotCommand
+    {
+        public RemindProductRoadmapTeam()
+            : base(
+                "remind product roadmap team",
+                "pbot remind product roadmap team - Reminds the product roadmap team when more features needs to be approved")
+        {
+        }
+
+        public override async Task Execute(string[] parameters, IResponse response)
+        {
+            var client = GitHubClientBuilder.Build();
+
+            var issueFilter = new RepositoryIssueRequest
+            {
+                State = ItemState.Open
+            };
+
+            var issues = await client.Issue.GetForRepository("Particular", "Requirements", issueFilter);
+
+            if (issues.Count(i => i.Labels.Any(l => l.Name == "Requirements: Approved")) < 2)
+            {
+                await response.Send(string.Format("@channel Hi there! I've noticed that there isn't enough approved requirements, please move more to the `approved` state. You can read more about the requirement process here: {0}",
+                    @"https://github.com/Particular/Requirements/wiki/Requirements-process")).IgnoreWaitContext();
+
+            }
+
+            if (issues.Count(i => i.Labels.Any(l => l.Name == "Needs: Customer impact")) < 5)
+            {
+                await response.Send(string.Format("@channel Hi all! I've noticed that there isn't enough requirements in the `customer impact assessment` state (5 seems like a good number), let's get more high prio issues over there?. You can read more about the requirement process here: {0}",
+                    @"https://github.com/Particular/Requirements/wiki/Requirements-process")).IgnoreWaitContext();
+
+            }
+
+        }
+    }
+}
