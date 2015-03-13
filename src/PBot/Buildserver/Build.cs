83413f914cf1b5ac77de7bd2a288a433a3567b18
Andreas Öhlund <andreas.ohlund@particular.net>
2015-03-13 13:29:11 +0100

    Fix R# errors

--- src/PBot/Buildserver/Build.cs
+++ src/PBot/Buildserver/Build.cs
@@ -28,7 +28,7 @@
             {
                 return true;
             }
-            if (obj.GetType() != this.GetType())
+            if (obj.GetType() != GetType())
             {
                 return false;
             }
