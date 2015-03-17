8edd05d9f6766265bf28a003e69b9b7eb363fff7
Andreas Öhlund <andreas.ohlund@particular.net>
2015-03-17 13:12:55 +0100

    Notify caretakers of broken builds

---
+++ src/PBot.Tests/NotifyCaretakersOfCurrentlyFailedBuildsTests.cs
@@ -0,0 +1,54 @@
+﻿namespace PBot.Tests
+{
+    using System.Linq;
+    using IssueButler.Mmbot.Repositories;
+    using NUnit.Framework;
+    using PBot.Buildserver;
+
+    class NotifyCaretakersOfCurrentlyFailedBuildsTests : BotCommandFixture<NotifyCaretakersOfCurrentlyFailedBuilds>
+    {
+        [Test]
+        public void RepoWithExistingBuild()
+        {
+            var repos = new AvailableRepositories
+            {
+                new AvailableRepositories.Repository
+                {
+                    Name = "ServiceControl",
+                    Caretaker = "johndoe"
+                },
+                 new AvailableRepositories.Repository
+                {
+                    Name = "ServiceInsight",
+                    Caretaker = "johndoe"
+                }
+
+                
+            };
+
+            brain.Set(repos);
+
+            Execute("notify caretakers of failed builds");
+        }
+
+        [Test]
+        public void RepoNoBuild()
+        {
+            var repos = new AvailableRepositories
+            {
+                new AvailableRepositories.Repository
+                {
+                    Name = "NoBuild",
+                    Caretaker = "johndoe"
+                },
+            
+            };
+
+            brain.Set(repos);
+
+            Execute("notify caretakers of failed builds");
+
+            Assert.True(Messages.First().Contains("help create"));
+        }
+    }
+}
---
+++ src/PBot/Buildserver/NotifyCaretakersOfCurrentlyFailedBuilds.cs
@@ -0,0 +1,66 @@
+namespace PBot.Buildserver
+{
+    using System.Linq;
+    using System.Threading.Tasks;
+    using IssueButler.Mmbot.Repositories;
+
+    public class NotifyCaretakersOfCurrentlyFailedBuilds : BotCommand
+    {
+        public NotifyCaretakersOfCurrentlyFailedBuilds()
+            : base(
+                "notify caretakers of failed builds$",
+                "notify caretakers of failed builds - Send a private message to each caretaker if there are currently failed builds")
+        {
+            client = new TeamCity("http://builds.particular.net");
+        }
+
+        public override async Task Execute(string[] parameters, IResponse response)
+        {
+            var reposGroupedByCaretaker = Brain.Get<AvailableRepositories>()
+                .Where(r => r.Caretaker != null)
+                .GroupBy(r => r.Caretaker)
+                .ToList();
+
+            var allBuilds = client.ListProjects()
+                .Select(p=>p.Name)
+                .ToList();
+
+            foreach (var repoGroup in reposGroupedByCaretaker)
+            {
+                var reposWithNoBuild = repoGroup.Where(r => !allBuilds.Contains(r.Name)).ToList();
+
+                if (reposWithNoBuild.Any())
+                {
+                    await response.Send(string.Format("Hi there @{0}! It seems that there is no build setup for {1} Just type `help create` if you want to help setting it up?", repoGroup.Key, string.Join(", ", reposWithNoBuild))).IgnoreWaitContext();                 
+                }
+
+                var reposWithABuild = repoGroup.Where(r => allBuilds.Contains(r.Name)).ToList();
+
+
+                var reposWithFailedBuilds = reposWithABuild.Where(r => HasFailedBuilds(r.Name))
+                    .ToList();
+
+                if (reposWithFailedBuilds.Any())
+                {
+                    await response.Send(string.Format("Hi there @{0}! It seems that the build is broken for {1} and since you're the caretaker I thought you would like to be aware. Just type `list failing builds for my repos` to get the details!", repoGroup.Key, string.Join(", ", reposWithFailedBuilds))).IgnoreWaitContext();
+                }
+            }
+        }
+
+        bool HasFailedBuilds(string name)
+        {
+       
+            var tcProj = client.GetProject(name);
+
+            return client.ListCurrentBuilds(tcProj.Id, new[]
+            {
+                "master",
+                "develop"
+            }).Any(b => b.Status == BuildStatus.Failed);
+
+
+        }
+
+        TeamCity client;
+    }
+}
