084cc805992501f9921f1b0717108a7efe09b20e
indualagarsamy <indu.alagarsamy@gmail.com>
2015-03-17 23:09:37 -0700

    Fixes #24

---
+++ src/PBot.Tests.Integration/RemindWhenMoreRequirementsNeedsToBeReviewedTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using PBot.Requirements;
+using PBot.Tests;
+
+public class RemindWhenMoreRequirementsNeedsToBeReviewedTests : GitHubIntegrationTest
+{
+
+    [Test]
+    public async void Should_remind_when_there_is_more_than_1_requirements_to_be_reviewed()
+    {
+        var response = new TestResponder();
+        await GitHubClient.Issue.Create(RepositoryOwner, RepositoryName, Requirements.NewFeature("Approved1", state: RequirementStates.Approved));
+        await GitHubClient.Issue.Create(RepositoryOwner, RepositoryName, Requirements.NewFeature("Approved1", state: RequirementStates.Review));
+        var chore = new RemindWhenMoreRequirementsNeedsToBeReviewed(GitHubClient, Repository, response);
+        await chore.Perform();
+        Assert.AreEqual(1, response.Messages.Count);
+    }
+
+    [Test]
+    public async void Should_not_remind_when_there_are_no_requirements_to_be_reviewed()
+    {
+        var response = new TestResponder();
+        var chore = new RemindWhenMoreRequirementsNeedsToBeReviewed(GitHubClient, Repository, response);
+        await GitHubClient.Issue.Create(RepositoryOwner, RepositoryName, Requirements.NewFeature("Approved1", state: RequirementStates.Approved));
+        await chore.Perform();
+        Assert.AreEqual(0, response.Messages.Count);
+    }
+}
---
+++ src/PBot/Requirements/RemindWhenMoreRequirementsNeedsToBeReviewed.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Octokit;
+
+namespace PBot.Requirements
+{
+    using System.Text;
+
+    public class RemindWhenMoreRequirementsNeedsToBeReviewed
+    {
+        readonly IGitHubClient client;
+        readonly Repository repository;
+        readonly BotCommand.IResponse response;
+
+        public RemindWhenMoreRequirementsNeedsToBeReviewed(IGitHubClient client, Repository repository, BotCommand.IResponse response)
+        {
+            this.client = client;
+            this.repository = repository;
+            this.response = response;
+        }
+
+        public async Task Perform()
+        {
+
+            var allIssues = await client.Issue.GetForRepository(repository.Owner.Login, repository.Name);
+            var issuesToBeReviewed = allIssues.Where(i => i.Labels.Any(l => l.Name == RequirementStates.Review)).ToList();
+
+            if (issuesToBeReviewed.Count > 0)
+            {
+                var sb = new StringBuilder();
+                sb.AppendLine(string.Format("There are {0} issue(s) waiting to be approved in requirements. Please review the following issues:", issuesToBeReviewed.Count));
+                foreach (var issue in issuesToBeReviewed)
+                {
+                    sb.AppendLine(issue.HtmlUrl.ToString());
+                }
+                await response.Send(sb.ToString());
+            }
+        }
+    }
+}
