beae5eec11646bcf5d4a3cc852394fcb5675ef69
Andreas Öhlund <andreas.ohlund@particular.net>
2015-05-04 16:25:16 +0200

    More comment tweaks

--- src/PBot/Requirements/MoveStaleInProgressRequirementsToApproved.cs
+++ src/PBot/Requirements/MoveStaleInProgressRequirementsToApproved.cs
@@ -32,19 +32,17 @@
                     await client.Issue.Labels.RemoveFromIssue(repository.Owner.Login, repository.Name, issue.Number, RequirementStates.InProgress);
                     await client.Issue.Labels.AddToIssue(repository.Owner.Login, repository.Name, issue.Number, new[] { (string)RequirementStates.Approved });
 
-                    var message = string.Format(
-@"
-There hasn't been any progress on this one in the last {0} days so I've moved it back to `Approved`.
+                    var message = @"
+During the last week I haven't noticed any updates either as comment or as items in the `Plan of attack` section beeing changed or completed. I've therefor decided to moved it back to `Approved`.
 
 * Do you think this requirement is stuck and need help? 
-* Was there progress and you think I did the wrong thing? Sorry but you're wrong :) During a week there should be at least a few updates either as comments or as items in the `Plan of attack` section?
-* Has priorities changed? Is this one still relevant?
+* Has priorities changed? 
+* Is this one still relevant?
 
 Move it back to `In progress` when work on this one resumes again!
 
-
 Ping @particular/requirements or the #requirements slack channel if you need a hand to reevalutate this requirement!
-", maxStaleness.TotalDays);
+";
 
                     await client.Issue.Comment.Create(repository.Owner.Login, repository.Name, issue.Number, message);
                 }
