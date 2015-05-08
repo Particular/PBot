d8de6007ee987bd2abbcf053132bd58b1e64a486
Sean Feldman <feldman.sean@gmail.com>
2015-05-08 14:37:12 -0600

    Fixes #32

---
+++ src/PBot/Issues/LabelUtility.cs
@@ -0,0 +1,28 @@
+﻿namespace PBot.Issues
+{
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Octokit;
+
+    class LabelUtility
+    {
+        public static async Task<bool> RepositoryHasLabels(RepoInfo repo, params Label[] labels)
+        {
+            var client = GitHubClientBuilder.Build();
+            var existingLabels = await client.Issue.Labels.GetForRepository(repo.Owner, repo.Name);
+ 
+            foreach (var label in labels)
+            {
+                var foundLabel = existingLabels.SingleOrDefault(l => l.Name.Equals(label.Name, StringComparison.InvariantCulture));
+
+                if (foundLabel == null || foundLabel.Color != label.Color)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
