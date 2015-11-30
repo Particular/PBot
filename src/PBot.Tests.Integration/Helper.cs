7d1d94fdbe4f52bccdf49291265df6a06da5207c
Simon Cropp <simon.cropp@gmail.com>
2015-12-01 00:19:46 +1100

    cleanup

--- src/PBot.Tests.Integration/Helper.cs
+++ src/PBot.Tests.Integration/Helper.cs
@@ -8,16 +8,13 @@ public static class Helper
 {
     static readonly Lazy<Credentials> _credentialsThunk = new Lazy<Credentials>(() =>
     {
-        var githubUsername = Environment.GetEnvironmentVariable("PBOT_GITHUBUSERNAME");
-        UserName = githubUsername;
-
         var githubToken = Environment.GetEnvironmentVariable("PBOT_OAUTHTOKEN");
 
         if (githubToken != null)
             return new Credentials(githubToken);
 
         var githubPassword = Environment.GetEnvironmentVariable("PBOT_GITHUBPASSWORD");
-
+        var githubUsername = Environment.GetEnvironmentVariable("PBOT_GITHUBUSERNAME");
         if (githubUsername == null || githubPassword == null)
             return null;
 
@@ -32,12 +29,8 @@ public static class Helper
         Debug.WriteIf(Credentials == null, "No credentials specified.");
     }
 
-    public static string UserName { get; private set; }
 
-    public static Credentials Credentials => _credentialsThunk.Value;
-
-    //todo: do we create a ParticularTest org that's paid?
-    public static bool IsPaidAccount => !string.IsNullOrWhiteSpace(Environment.GetEnvironmentVariable("PBOT_PRIVATEREPOSITORIES"));
+    static Credentials Credentials => _credentialsThunk.Value;
 
     public static void DeleteRepo(Repository repository)
     {
