cd00cda51561d077b8f54b36f5a80685312c0275
danielmarbach <daniel.marbach@openplace.net>
2016-10-26 08:56:19 +0200

    Sync has ability to sync license

--- src/PBot/SyncOMatic/DefaultTemplateRepo.cs
+++ src/PBot/SyncOMatic/DefaultTemplateRepo.cs
@@ -5,11 +5,10 @@ namespace PBot.SyncOMatic
 
     public static class DefaultTemplateRepo
     {
-        public static List<SyncItem> ItemsToSync = new List<SyncItem>();
+        static List<SyncItem> ItemsToSync = new List<SyncItem>();
 
         static DefaultTemplateRepo()
         {
-
             ItemsToSync.Add(new SyncItem
             {
                 Parts = new Parts("Particular/RepoStandards", TreeEntryTargetType.Blob, "master", ".gitignore")
@@ -30,6 +29,23 @@ namespace PBot.SyncOMatic
                 Target = "{{src.root}}/{{solution.name}}.sln.DotSettings",
                 Parts = new Parts("Particular/RepoStandards", TreeEntryTargetType.Blob, "master", "src/RepoName.sln.DotSettings")
             });
+        }
+
+        public static List<SyncItem> WithLicense(string license)
+        {
+            if (license == null)
+            {
+                return ItemsToSync;
+            }
+
+            var items = new List<SyncItem>(ItemsToSync);
+            var licenseFile = license != "standard" ? $"{license.ToUpper()}.md" : "RPL.md";
+            items.Add(new SyncItem
+            {
+                Target = "LICENSE",
+                Parts = new Parts("Particular/RepoStandards", TreeEntryTargetType.Blob, "master", licenseFile)
+            });
+            return items;
         }
     }
 }
